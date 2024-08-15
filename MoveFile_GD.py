bcad20f7ee7403943a5bec8425de58f072193898
roger2234 <roger2234@gmail.com>
2024-08-16 02:37:54 +0800

    Update: replace v3 to MoveFile_GD_v6.py

--- MoveFile_GD.py
+++ MoveFile_GD.py
@@ -2,6 +2,7 @@ import pygsheets
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from itertools import zip_longest
 
 # 設定 Google Sheets API 和 Google Drive API 認證
 SHEET_SERVICE_ACCOUNT_FILE = 'D:/DMM_list/rk-dmm-scrapying@dmm-scrapying.iam.gserviceaccount.com/dmm-scrapying-ca95a23a4216.json'  # 使用者A的Sheet API憑證
@@ -10,6 +11,7 @@ DRIVE_SERVICE_ACCOUNT_FILE = 'D:/DMM_list/rk-dmm-scrapying@dmm-scrapying.iam.gse
 # Google Sheets 認證
 sheet_credentials = Credentials.from_service_account_file(SHEET_SERVICE_ACCOUNT_FILE)
 sheet_client = pygsheets.authorize(service_account_file=SHEET_SERVICE_ACCOUNT_FILE)
+ # 填入要讀取及編輯的Google Sheet
 spreadsheet_id = '1CSJW28pvLHj9w3L1fiJhZp3opGdHLPdq1EdhqwaqIzs'
 sh = sheet_client.open_by_key(spreadsheet_id)
 
@@ -17,11 +19,11 @@ sh = sheet_client.open_by_key(spreadsheet_id)
 drive_credentials = Credentials.from_service_account_file(DRIVE_SERVICE_ACCOUNT_FILE)
 drive_service = build('drive', 'v3', credentials=drive_credentials)
 
-# 設定 Team Drive ID 和目標資料夾 ID
+# 設定 Team Drive ID 和目標資料夾 ID，這邊都為Team Drive根目錄的ID
 TEAM_DRIVE_ID = '0AOj7YJoxqthlUk9PVA'
 PARENT_FOLDER_ID = '0AOj7YJoxqthlUk9PVA'  # 根據需要設定搜尋和創建資料夾的目標資料夾ID
 
-# 創建資料夾
+# 建立資料夾
 def create_folder(name, parent_id=None):
     try:
         folder_metadata = {
@@ -54,7 +56,7 @@ def search_folder(name, parent_id=None):
         items = results.get('files', [])
         return items
     except HttpError as error:
-        print(f'An error occurred while searching folder: {error}')
+        print(f'搜尋資料夾時發生錯誤: {error}')
         return []
     
 # 搜尋檔案
@@ -67,7 +69,7 @@ def search_file(name, parent_id=None):
         items = results.get('files', [])
         return items
     except HttpError as error:
-        print(f'An error occurred while searching file: {error}')
+        print(f'搜尋檔案時發生錯誤: {error}')
         return []
 
 # 移動檔案
@@ -77,12 +79,9 @@ def move_file(file_id, folder_id):
         if not file:
             print(f"檔案 '{file_id}' 不存在，跳過移動。")
             return
-        # 先獲取檔案的父資料夾
-        #file = drive_service.files().get(fileId=file_id, fields='parents').execute()
         
         previous_parents = ",".join(file.get('parents'))
-        # 移動檔案到新資料夾
-        #file = drive_service.files().update(fileId=file_id, addParents=folder_id, removeParents=previous_parents, fields='id, parents', supportsAllDrives=True).execute()
+
         drive_service.files().update(
             fileId=file_id,
             addParents=folder_id,
@@ -91,66 +90,128 @@ def move_file(file_id, folder_id):
             supportsAllDrives=True
         ).execute()
     except HttpError as error:
-        print(f'An error occurred while moving file: {error}')
+        print(f'移動檔案時發生錯誤: {error}')
 
+# 利用資料夾ID取得資料夾名稱
+def get_folder_name_by_id(folder_id):
+    try:
+        # 使用 Drive API 取得資料夾的元資料(Metadata)
+        folder = drive_service.files().get(fileId=folder_id, fields='id, name', supportsAllDrives=True).execute()
+        # 取得資料夾名稱
+        folder_name = folder.get('name')
+        return folder_name
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        return None
+    
 # 主程式
 def main():
     # 讀取試算表中的所有分頁名稱
     worksheets = sh.worksheets()
-    exclude_sheets = ["女優列表", "あいだゆあ", "高井桃", "高樹マリア", "川島和津実", "桜朱音"]  # 在此列表中加入要排除的分頁名稱
-    #team_drive_root_id = "0AOj7YJoxqthlUk9PVA"
+    print(f"目前所有存在工作表： {worksheets}") 
+
+    # 設定排除工作表
+    exclude_sheets = ["讀我", "女優列表", "TempList", "波多野手動刪除的資料", "小泉彩RK已下載", "「桜朱音」RK已下載", "「さくら林檎」勞贖處理過", "愛田由PBList", "橘れもん_Old", "早乙女ルイ_old",
+                      "水卜さくら", "河北彩花", "さくら林檎", "白石ひとみ", "相沢梨菜", "及川奈央", "堤さやか", "松島かえで", "小泉彩", "里美ゆりあ", "高井桃", "持月真由", "穂花(wrong)", "美織", "赤井美月", "篠宮ゆり", "あやみ旬果", "上原亜衣", "桐谷ユリア", "絵色千佳", "美雪ありす", "北川瞳",
+                      "小倉奈々", "水嶋あずみ", "椎名ゆな", "桜ここみ", "あいだゆあ", "RIONA(りおな)", "成瀬心美", "水原あき", "山手栞", "くるみひな", "杏樹紗奈", "穂花", "小倉ゆず", "鈴木心春", "松岡ちな", "愛乃なみ", "桃谷エリカ", "桐原エリカ", "麻倉憂", "桜ここみ", "希志あいの", "大橋未久",
+                      "折原ほのか", "高樹マリア", "鮎川あみ", "桜朱音", "川島和津実", "かすみ果穂", "橘くらら", "冬月かえで", "前嶋美歩", "香坂百合", "一ノ瀬アメリ", "星野あかり", "神谷姫", "神谷美雪", "小沢菜穂", "美竹涼子", "峰なゆか", "稲森しほり", "菅野亜梨沙", "さとう遥希", "早乙女ルイ", "鈴村あいり", "果梨",
+                      "椎名そら",
+                      "波多野結衣"]  # 在此列表中加入要排除的分頁名稱
+    
+    #print(f"排除工作表： {exclude_sheets}") 
 
     for worksheet in worksheets:
         actress_name = worksheet.title
-        print(actress_name)
+        print(f"目前處理工作表： {actress_name}")
         if actress_name in exclude_sheets:
-            print(f"跳過分頁：{actress_name}")
+            print(f"跳過此工作表： {actress_name}")
             continue
         
-        # 創建 "非個人作品" 資料夾
+        # 建立 "非個人作品" 資料夾
         non_personal_folder_name = f"{actress_name}_非個人作品"
         existing_non_personal_folders = search_folder(non_personal_folder_name, PARENT_FOLDER_ID)
         if existing_non_personal_folders:
-            print(f"資料夾 '{non_personal_folder_name}' 已存在，跳過創建。")
+            print(f"資料夾： '{non_personal_folder_name}'  已存在，跳過建立資料夾。")
             non_personal_folder_id = existing_non_personal_folders[0]['id']
         else:
             non_personal_folder_id = create_folder(non_personal_folder_name, PARENT_FOLDER_ID)
         
-        # 創建 "個人作品" 資料夾
+        # 建立 "個人作品" 資料夾
         personal_folder_name = f"{actress_name}_個人作品"
         existing_personal_folders = search_folder(personal_folder_name, PARENT_FOLDER_ID)
         if existing_personal_folders:
-            print(f"資料夾 '{personal_folder_name}' 已存在，跳過創建。")
+            print(f"資料夾： '{personal_folder_name}'  已存在，跳過建立資料夾。")
             personal_folder_id = existing_personal_folders[0]['id']
         else:
             personal_folder_id = create_folder(personal_folder_name, PARENT_FOLDER_ID)
         
         if personal_folder_id and non_personal_folder_id:
-            # 獲取該分頁中 C 欄和 H 欄的所有值（從 C2 和 H2 開始）
-            video_codes = worksheet.get_col(3, include_tailing_empty=False)[1:]
-            tags = worksheet.get_col(8, include_tailing_empty=False)[1:]
+            # 獲取該分頁中C欄"品番"的值
+            video_codes = worksheet.get_col(5, include_tailing_empty=False)[1:]
 
-            for code, tag in zip(video_codes, tags):
+            # 讀取G欄"単体作品"的值
+            single_tags = worksheet.get_col(9, include_tailing_empty=False)[1:]
+
+            # 讀取H欄"ベスト・総集編"的值
+            nonsingle_tags = worksheet.get_col(10, include_tailing_empty=False)[1:]
+ 
+            # 取得品番數量，即影片數量。
+            num_video_codes = len(video_codes)
+            print(f"影片(品番)數量為：{num_video_codes}")
+
+            for row, (code, single_tag, nonsingle_tag) in enumerate(zip_longest(video_codes, single_tags, nonsingle_tags), start = 2):
+                if not single_tag:  # 處理空字串或None
+                    single_tag = ""
+                if not nonsingle_tag:  # 處理空字串或None
+                    nonsingle_tag = ""
+                print(f"處理第'{row}'列，品番: {code}, I欄: {single_tag}, J欄: {nonsingle_tag}")
+                
                 if not code:
+                    print(f"Skipping row {row} because the code is empty.")
                     continue
                 file_name = f"{code}.mp4"
-                print(file_name)
+                print(f"Searching for file: {file_name}")
+
                 # 搜尋檔案
                 files = search_file(file_name)
-                print(files)
-                for file in files:
-                    if tag == "ベスト・総集編":
-                        target_folder_id = non_personal_folder_id
-                    else:
-                        target_folder_id = personal_folder_id
+
+                # 寫入到以"actress_name"命名的.txt文件
+                with open(f"{actress_name}處理過的品番.txt", "a", encoding="utf-8") as f:
+                    f.write(f"{file_name.replace('.mp4', '')}\n")
+
+                # 如果沒有搜尋到該檔案，則在A欄下載者標記。
+                cell_value = worksheet.cell(f'A{row}').value
+
+                if not files:
+                    print(f" 沒有找到檔案： '{file_name}'。")
+                    if cell_value == '':
+                        worksheet.update_value(f'A{row}', 'Not in PB')
+                        print(f"更新 'A{row}' 資料為： Not in PB 。")
+                    continue
+
+                for file in files:                    
+                    print(f"找到檔案: {file['name']}, ID: {file['id']}")
+
+                    # 如果I欄為"単体作品"，或者是J欄為空，則移動到個人作品，其他則丟到非個人作品。
+                    target_folder_id = personal_folder_id if single_tag == "単体作品" or  nonsingle_tag == "" else non_personal_folder_id
+                    print(f"品番：{code}, 単体作品為 '{single_tag}', ベスト・総集編為 '{nonsingle_tag}'")
+                    
+                    # 根據Folder ID取得資料夾名稱
+                    target_folder_name = get_folder_name_by_id(target_folder_id)
 
                     if target_folder_id in file.get('parents', []):
-                        print(f"檔案 '{file_name}' 已存在於資料夾中，跳過移動。")
+                        print(f"檔案 '{file_name}' 已存在於資料夾：'{target_folder_name}' 中，跳過移動。")
                     else:
                         move_file(file['id'], target_folder_id)
-                        print(f"已將檔案 '{file_name}' 移動到資料夾。")
+                        print(f"已將檔案 '{file_name}' 移動到資料夾：'{target_folder_name}'。")
+
+                    print(f"檔案 '{file_name}' 已存在或被移動到'{target_folder_name}' 。")
+           
+                    if cell_value == '':
+                        worksheet.update_value(f'A{row}', target_folder_name)
+                        print(f"更新 'A{row}' 資料為： {target_folder_name} 。")
         else:
-            print(f"無法創建或取得資料夾的 ID。")
+            print(f"無法建立或取得資料夾的 ID。")
 
 if __name__ == "__main__":
     main()
